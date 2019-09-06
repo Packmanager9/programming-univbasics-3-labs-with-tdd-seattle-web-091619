c4a615be968f889b819aa460acd989687f6eb4d4
Cole Ditzler <colejditzler@gmail.com>
2019-09-06 22:25:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,5 @@ first_number = 1;
 second_number = 2;# Add your variables here
 sum = first_number + second_number;
 difference = first_number - second_number;
+product = first_number * second_number;
+
